9dd802f34f7def343c6776636c45fa50f8f7704d
Balázs András <xeropax@gmail.com>
2016-01-02 14:01:36 +0000

    completed

--- targyfelvetel/app/pods/subjects/new/controller.js
+++ targyfelvetel/app/pods/subjects/new/controller.js
@@ -3,22 +3,30 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
    actions: {
         submit(){
-			var subject = this.store.createRecord('subject',
-                    {
-                        name: this.get('name'),
-                        code: this.get('code'),
-                        size: this.get('size'),
-                        location: this.get('location'),
-                        teacher: this.get('teacher'),
-                    } );
-			subject.save();
-			this.set('name','');
-			this.set('code','');
-			this.set('size','');
-			this.set('location','');
-			this.set('teacher','');
-			this.transitionToRoute('/subjects/list');
-			
+        	if(document.getElementById('name').value == "" ||
+        	document.getElementById('code').value == "" ||
+        	document.getElementById('size').value == "" ||
+        	document.getElementById('location').value == "" ||
+        	document.getElementById('teacher').value == "" ){
+        		alert("Az űrlap nincs teljesen kitöltve!");
+        	}
+        	else{
+				var subject = this.store.createRecord('subject',
+	                    {
+	                        name: this.get('name'),
+	                        code: this.get('code'),
+	                        size: this.get('size'),
+	                        location: this.get('location'),
+	                        teacher: this.get('teacher'),
+	                    } );
+				subject.save();
+				this.set('name','');
+				this.set('code','');
+				this.set('size','');
+				this.set('location','');
+				this.set('teacher','');
+				this.transitionToRoute('/subjects/list');
+        	}
         },
 		clear(){
 			this.set('name','');
--- targyfelvetel/app/pods/subjects/update/controller.js
+++ targyfelvetel/app/pods/subjects/update/controller.js
@@ -3,20 +3,29 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions:{
 		submit(){
-			var subject = this.get('model');
-			subject.setProperties( { 
-			    name: document.getElementById('name').value, 
-			    code: document.getElementById('code').value,
-			    size: document.getElementById('size').value,
-			    location: document.getElementById('location').value,
-			    teacher: document.getElementById('teacher').value
-			})
-			return subject.save().then(() => {
-                this.transitionToRoute('index');
-            });
+	    	if(document.getElementById('name').value == "" ||
+        		document.getElementById('code').value == "" ||
+        		document.getElementById('size').value == "" ||
+        		document.getElementById('location').value == "" ||
+        		document.getElementById('teacher').value == "" ){
+        			alert("Az űrlap nincs teljesen kitöltve!");
+        	}
+        	else{
+				var subject = this.get('model');
+				subject.setProperties( { 
+				    name: document.getElementById('name').value, 
+				    code: document.getElementById('code').value,
+				    size: document.getElementById('size').value,
+				    location: document.getElementById('location').value,
+				    teacher: document.getElementById('teacher').value
+				})
+				return subject.save().then(() => {
+	                this.transitionToRoute('/subjects/list');
+	            });
+        	}
 		},
-		clear(){
-			this.transitionToRoute('index');
+		back(){
+			this.transitionToRoute('/subjects/list');
 		},
 	}
 });

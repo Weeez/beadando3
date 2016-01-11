aa901d0b7c850aa44b7ff9779753ea7ff0d3f2bf
Balázs András <xeropax@gmail.com>
2016-01-11 18:28:09 +0000

    final

--- targyfelvetel/app/router.js
+++ targyfelvetel/app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login', {path: '/login'});
+  this.route('/', {path: '/index'});
   this.route('registration', {path: '/registration'});
   this.route('about', {path: '/about'});
   this.route('subjects', function() {
